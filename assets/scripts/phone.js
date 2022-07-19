a184a8a19302cc5348c796b727ffae2cbd1b745c
Diego de souza <diegodesouza.souza@gmail.com>
2022-07-18 21:02:16 -0300

    adição de video

--- assets/scripts/phone.js
+++ assets/scripts/phone.js
@@ -8,3 +8,38 @@ function mascara(telefone){
         telefone.value = telefone.value + '-';
 
 }
+
+
+var btnplay = document.getElementById('btnplay');
+var btnSom = document.getElementById('btnSom');
+var videoON = document.querySelector("#player");
+
+function playPause() {
+    if(videoON.paused){
+        btnplay.style.backgroundImage = `url('../img/botao-de-pausa.png')`;
+        videoON.play();
+    }else {
+        btnplay.style.backgroundImage = `url('../img/btn_play.png')`;
+        videoON.pause();
+    }
+}
+
+function somAudioVideo(){
+    
+    if(videoON.paused){
+        btnSom.style.backgroundImage = `url('../img/alto-falante-on.png')`;
+    }else {
+        if(videoON.muted==false){
+            btnSom.style.backgroundImage = `url('../img/alto-falante-off.png')`;
+            videoON.muted = true;
+        } else {
+            btnSom.style.backgroundImage = `url('../img/alto-falante-on.png')`;
+            videoON.muted = false;
+        }
+    }
+    console.log(estadoImagem)
+}
+
+
+
+
